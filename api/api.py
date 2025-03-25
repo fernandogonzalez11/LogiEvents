695c703a87f5e5715bdc5a2d651d8cea5f6b9736
fernandogonzalez11 <fernandogonzalezeducativo@gmail.com>
2025-03-24 22:11:32 -0600

    test endpoint

--- api/api.py
+++ api/api.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("../html/login/index.html")
 
+@app.route("/test/")
+def test():
+    return "works"
+
 if __name__ == "__main__":
     app.run()
