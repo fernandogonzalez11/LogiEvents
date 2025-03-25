02666d397278fe83b41202ccf89d069f690adb0e
fernandogonzalez11 <fernandogonzalezeducativo@gmail.com>
2025-03-24 22:22:21 -0600

    different file structure

--- api.py
+++ api.py
@@ -1,18 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="html")
 
 @app.route("/")
 def home():
-    return render_template("../html/login/index.html")
+    return render_template("login/index.html")
 
 @app.route("/test/")
 def test():
     return "works"
-
-@app.route("/test2/")
-def test2():
-    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run()
