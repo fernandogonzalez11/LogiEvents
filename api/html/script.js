b6b17e7e5ae7dd954135afa0afd30922a44288ba
Serzc <serzc.cr@gmail.com>
2025-03-26 22:00:47 -0600

    Conectar botones y páginas
    En api/html/script.js están las funciones que llevan a las páginas, si falta alguno, solamente hay que agregar
    <script defer src="../script.js"></script>
    al inicio del archivo html y llamar la función con onclick="goToX()"

---
+++ api/html/script.js
@@ -0,0 +1,27 @@
+function goToAdminPanel() {
+    window.location.href='/admin-panel/';
+}
+function goToCreateEvent() {
+    window.location.href='/create-event/page1/';
+}
+function goToCreateEvent2() {
+    window.location.href='/create-event/page2/';
+}
+function goToEditEvent() {
+    window.location.href='/edit-event/';
+}
+function goToEditProfileAdmin() {
+    window.location.href='/edit-profile/admin/';
+}
+function goToEditProfileUser() {
+    window.location.href='/edit-profile/user/';
+}
+function goToEvents() {
+    window.location.href='/events/';
+}
+function goToLogin() {
+    window.location.href='/login/';
+}
+function goToReserveEvent() {
+    window.location.href='/reserve-event/';
+}
