bf375659b6ded83996dea657d49c6bd5487b922e
fernandogonzalez11 <fernandogonzalezeducativo@gmail.com>
2025-04-03 15:17:03 -0600

    place formats in the user input validation messages

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,15 @@
 const { validateCedula, validateEmail, validateEmployeeID, validatePhone, validateUsername } = require("../controller/validation");
+const { USERNAME_MAX_LENGTH } = require('./Constants');
 
 class User {
     constructor({ id = null, cedula, name, mail, phone, username, password, type, rol = null, id_empleado = null }) {
       if (!name.length) throw new Error("Nombre inválido");
-      if (!validateCedula(cedula)) throw new Error("Cédula inválida");
-      if (!validateEmail(mail)) throw new Error("Email inválido");
-      if (!validatePhone(phone)) throw new Error("Número de teléfono inválido");
-      if (!validateUsername(username)) throw new Error("Usuario inválido");
-      if (rol && !rol.length) throw new Error("Rol inválido");
-      if (id_empleado && !validateEmployeeID(id_empleado)) throw new Error("ID de empleado inválido");
+      if (!validateCedula(cedula)) throw new Error("Cédula inválida (formato: 102340567");
+      if (!validateEmail(mail)) throw new Error("Email inválido (formato: usuario@pagina.com)");
+      if (!validatePhone(phone)) throw new Error("Número de teléfono inválido (formato: 12345678)");
+      if (!validateUsername(username)) throw new Error(`Usuario inválido (debe ser máximo ${USERNAME_MAX_LENGTH} carácteres`);
+      if (rol && !rol.length) throw new Error("Rol inválido (no debe ser vacío)");
+      if (id_empleado && !validateEmployeeID(id_empleado)) throw new Error("ID de empleado inválido (formato: EMP-123)");
   
       this.id = id;
       this.cedula = cedula;
